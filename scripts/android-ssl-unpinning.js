3eaa3c8a05feaaf2be7aa791f20e2a87126b2966
zxhubo <zxhubo@126.com>
2019-04-18 23:25:26 +0800

    Update android-ssl-unpinning.js

--- scripts/android-ssl-unpinning.js
+++ scripts/android-ssl-unpinning.js
@@ -1,1 +1,24 @@
 
+"use strict"
+
+setImmediate(function() {
+	if(Java.available){
+		Java.perform(function(){
+			/*
+			hook list:
+			1.SSLcontext
+			2.okhttp
+			3.webview
+			4.XUtils
+			5.httpclientandroidlib
+			6.JSSE
+			7.network\_security\_config (android 7.0+)
+			8.Apache Http client (support partly)
+			*/
+			console.log("[+] starting to unpinning!");
+
+		});
+
+	}
+});
+
