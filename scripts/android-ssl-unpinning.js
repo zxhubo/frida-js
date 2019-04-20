b37054972efb97d43546a16e2b7c8950999f352e
zxhubo <zxhubo@126.com>
2019-04-20 12:01:58 +0800

    Update android-ssl-unpinning.js

--- scripts/android-ssl-unpinning.js
+++ scripts/android-ssl-unpinning.js
@@ -16,6 +16,43 @@ setImmediate(function() {
 			8.Apache Http client (support partly)
 			*/
 			console.log("[+] starting to unpinning!");
+			try {
+
+				var class_name = "org.conscrypt.Platform";
+
+				var Platform  = Java.use(class_name);
+
+
+				Platform.checkServerTrusted.overload('javax.net.ssl.X509TrustManager', '[Ljava.security.cert.X509Certificate;', 'java.lang.String', 'org.conscrypt.ConscryptEngine').implementation = function(arg1,arg2,arg3,arg4){
+
+
+						console.log("[*]hook org.conscrypt.Platform.checkServerTrusted('javax.net.ssl.X509TrustManager', '[Ljava.security.cert.X509Certificate;', 'java.lang.String', 'org.conscrypt.ConscryptEngine') successful");
+				    return;
+				}
+
+
+				Platform.checkServerTrusted.overload('javax.net.ssl.X509TrustManager', '[Ljava.security.cert.X509Certificate;', 'java.lang.String', 'org.conscrypt.AbstractConscryptSocket').implementation = function(arg1,arg2,arg3,arg4){
+
+						console.log("[*]hook hook org.conscrypt.Platform.checkServerTrusted('javax.net.ssl.X509TrustManager', '[Ljava.security.cert.X509Certificate;', 'java.lang.String', 'org.conscrypt.AbstractConscryptSocket') successful");
+
+				    return;
+				}
+
+
+			} catch(e) {
+
+
+			     if (e.message.indexOf('ClassNotFoundException') != -1) {
+
+				 console.log(class_name + " not found!");
+			     } else {
+
+				throw new Error(e);
+
+			     }
+
+
+			}
 
 		});
 
