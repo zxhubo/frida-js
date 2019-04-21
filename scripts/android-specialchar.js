172f061b18c2e2ea721ae1e607c32d00192d348b
zxhubo <zxhubo@126.com>
2019-04-21 21:19:13 +0800

    Create android-specialchar.js

---
+++ scripts/android-specialchar.js
@@ -0,0 +1,21 @@
+Java.perform(
+        function x() {
+
+            var targetClass = "com.example.hooktest.MainActivity";
+
+            var hookCls = Java.use(targetClass);
+            var methods = hookCls.class.getDeclaredMethods();
+
+            for (var i in methods) {
+                console.log(methods[i].toString());
+                console.log(encodeURIComponent(methods[i].toString().replace(/^.*?\.([^\s\.\(\)]+)\(.*?$/, "$1")));
+            }
+
+            hookCls[decodeURIComponent("%D6%8F")]
+                .implementation = function (x) {
+                    console.log("original call: fun(" + x + ")");
+                    var result = this[decodeURIComponent("%D6%8F")](900);
+                    return result;
+                }
+        }
+    )
