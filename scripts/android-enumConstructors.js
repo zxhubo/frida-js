f9292858039d8d13d1d21b041951eb5a9e58321d
zxhubo <zxhubo@126.com>
2019-05-27 15:00:33 +0800

    Update android-enumConstructors.js

--- scripts/android-enumConstructors.js
+++ scripts/android-enumConstructors.js
@@ -1,2 +1,17 @@
 /* author:bobby *day:2019-05-27 */
 'use strict'
+
+var targetClass="xxx.xxx.xxx";
+var clz=Java.use(targetClass);
+var overloads = clz.$init.overloads; 
+overloads.forEach(function(overload) {                
+	var proto = "(";                
+	overload.argumentTypes.forEach(function(type){                    
+		proto += type.className + ", ";                
+	});                
+	if(proto.length > 1){                   
+ 		proto = proto.substr(0, proto.length - 2);                
+ 	}                
+	proto += ")";              
+	console.log("hooking "+targetClass+proto);
+});
