c9750cd8d84a8ad522a655288e0e2324f83b39b3
zxhubo <zxhubo@126.com>
2020-06-22 17:12:50 +0800

    Update android-enumMap.js

--- scripts/android-enumMap.js
+++ scripts/android-enumMap.js
@@ -1,10 +1,21 @@
+// author:bobby
 'use strict'
 
+// enumerate java.util.HashMap
 var hashMap = Java.use('java.util.HashMap');
 var hashMapNode = Java.use('java.util.HashMap$Node');
 var iterator = hashMap.entrySet().iterator();
 while(iterator.hasNext()){
 	var entry = Java.cast(iterator.next(),hashMapNode);
+	console.log(entry.getKey());
+	console.log(entry.getValue());
+}
+
+// enumerate java.util.HashMap
+var hashMap = Java.use('java.util.concurrent.ConcurrentHashMap');
+var iterator = hashMap.entrySet().iterator();
+while(iterator.hasNext()){
+	var entry = Java.cast(iterator.next(),Java.use("java.util.concurrent.ConcurrentHashMap$MapEntry"));
 	console.log(entry.getKey());
 	console.log(entry.getValue());
 }
