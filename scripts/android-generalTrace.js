90f502475d500353a982029c3a92f329c8b8b641
zxhubo <zxhubo@126.com>
2020-11-17 15:13:12 +0800

    Update android-generalTrace.js

--- scripts/android-generalTrace.js
+++ scripts/android-generalTrace.js
@@ -4,50 +4,111 @@
 
 'use strict';
 
-function traceMethod(className,methodName){
+function traceMethod(className,methodName,isPrintParameter,isNoneOp){
+	var target_class=Java.use(className);
+	var methods=target_class[methodName].overloads;
+		methods.forEach(function(method){
+		var argsType="('";
+		method.argumentTypes.forEach(function(type){                    
+		  argsType=argsType+type.className+"','";            
+		}); 
+		if (argsType.length >1) {
+		  argsType=argsType.substr(0, argsType.length - 2);
+		}
+		argsType=argsType+")"; 
 
-  var target_class=Java.use(className);
-  var methods=target_class[methodName].overloads;
-  methods.forEach(function(method){
-    var argsType="('";
-    method.argumentTypes.forEach(function(type){                    
-      argsType=argsType+type.className+"','";            
-    }); 
-    if (argsType.length >1) {
-      argsType=argsType.substr(0, argsType.length - 2);
-    }
-    argsType=argsType+")"; 
+
+	  	method.implementation=function(){
+	    console.warn("[***] Hook "+className+"."+methodName+argsType+" succeed ......");
+	    if(isPrintParameter & arguments.length>0){
+	      for (var j = 0; j < arguments.length; j++) {
+	            console.log(methodName+"->arg[" + j + "](argType:\""+method.argumentTypes[j].className+"\"): " + arguments[j]);
+
+	            // Object.getOwnPropertyNames(arguments[j]).forEach(function(key){
+
+	            //     console.log(key+": "+arguments[j][key]);
+
+	            // });
+
+	            // var keys=Object.keys(arguments[j]);
+	            // console.log(keys);
 
 
-    method.implementation=function(){
-      console.warn("Hook "+className+"."+methodName+argsType+" succeed ......");
-      if(arguments.length>0){
-        for (var j = 0; j < arguments.length; j++) {
-              console.log(methodName+"->arg[" + j + "](argType:\""+method.argumentTypes[j].className+"\"): " + arguments[j]);
-              // Object.getOwnPropertyNames(arguments[j]).forEach(function(key){
+	            if((arguments[j]!=null) & (Object.keys(arguments[j]).indexOf("getClass")>-1)){
+	            	// console.log(arguments[j].getClass());
+	            	switch(arguments[j].getClass().getName()){
+	            		case "android.content.Intent":
+	            		  console.log(decodeURIComponent(arguments[j].toUri(Java.use("android.content.Intent").URI_ALLOW_UNSAFE.value)));
+	            		  break;
 
-              //     console.log(key,arguments[j][key]);
+	            		default:
+	            		  break;
+	            	}
+	            }
 
-              // });
-              if(arguments[j]!=null){
-                switch(arguments[j].getClass().getName()){
-                  case "android.content.Intent":
-                    console.log(decodeURIComponent(arguments[j].toUri(Java.use("android.content.Intent").URI_ALLOW_UNSAFE.value)));
-                    break;
+	        }
+	    }
+	    getStackTrace();
+	    // 判断是否需要什么都不操作直接返回
+	    if(isNoneOp){
+	    	return;
+	    }
 
-                  default:
-                    break;
-                }
-              }
-
-          }
-      }
-      getStackTrace();
-      var ret= method.apply(this,arguments);
-      if(ret!=null||ret!=undefined){
-        console.log(methodName+" ret="+ret);
-        return ret;
-      }
-    }
-  });
+	    var ret= method.apply(this,arguments);
+	    if(ret!=null||ret!=undefined){
+	      console.log(methodName+" ret="+ret);
+	      return ret;
+	    }
+	  }
+	});
 }
+
+
+
+// function traceMethod(className,methodName){
+
+//   var target_class=Java.use(className);
+//   var methods=target_class[methodName].overloads;
+//   methods.forEach(function(method){
+//     var argsType="('";
+//     method.argumentTypes.forEach(function(type){                    
+//       argsType=argsType+type.className+"','";            
+//     }); 
+//     if (argsType.length >1) {
+//       argsType=argsType.substr(0, argsType.length - 2);
+//     }
+//     argsType=argsType+")"; 
+
+
+//     method.implementation=function(){
+//       console.warn("Hook "+className+"."+methodName+argsType+" succeed ......");
+//       if(arguments.length>0){
+//         for (var j = 0; j < arguments.length; j++) {
+//               console.log(methodName+"->arg[" + j + "](argType:\""+method.argumentTypes[j].className+"\"): " + arguments[j]);
+//               // Object.getOwnPropertyNames(arguments[j]).forEach(function(key){
+
+//               //     console.log(key,arguments[j][key]);
+
+//               // });
+//               if(arguments[j]!=null){
+//                 switch(arguments[j].getClass().getName()){
+//                   case "android.content.Intent":
+//                     console.log(decodeURIComponent(arguments[j].toUri(Java.use("android.content.Intent").URI_ALLOW_UNSAFE.value)));
+//                     break;
+
+//                   default:
+//                     break;
+//                 }
+//               }
+
+//           }
+//       }
+//       getStackTrace();
+//       var ret= method.apply(this,arguments);
+//       if(ret!=null||ret!=undefined){
+//         console.log(methodName+" ret="+ret);
+//         return ret;
+//       }
+//     }
+//   });
+// }
