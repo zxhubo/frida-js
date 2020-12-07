ad69cf76358e618de195d976b47597a393210d9d
zxhubo <zxhubo@126.com>
2020-12-07 09:52:22 +0800

    Update android_proxy.js

--- scripts/android_proxy.js
+++ scripts/android_proxy.js
@@ -2,3 +2,9 @@
 Java.perform(function() {
     Java.use('android.net.Proxy').setHttpProxySystemProperty(Java.use('android.net.ProxyInfo').buildDirectProxy('1.0.0.1', 8081));
 });
+
+
+
+var type = Java.use("java.net.Proxy$Type").valueOf("HTTP");
+var socket = Java.use("java.net.InetSocketAddress").$new("192.168.0.12",8088);
+var proxy = Java.use("java.net.Proxy").$new(type,socket);
